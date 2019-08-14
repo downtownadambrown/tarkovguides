49a36b25e027128d2658a7d0dbbb2a1a1c474fcd
Adam Brown <downtownadambrown@gmail.com>
2019-08-14 16:11:15 -0400

    better routing functionality with flexbox integrated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,49 @@ import './css/App.css';
 
 import Sample from '../src/components/Sample';
 import Login from '../src/components/Login.jsx';
-
+import MapsApp from '../src/components/MapsApp.jsx';
+import ModdingApp from '../src/components/ModdingApp';
+import KeysApp from '../src/components/KeysApp';
 import Home from '../src/components/Home';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import MainAppBar from "./components/MainAppBar";
+import Footer from "./components/Footer";
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    content: {
+        flex: "1 0 auto",
+        marginBottom: 10
+    },
+}));
 
 function App() {
-  return (
-      <Router>
-          <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/login" component={Login}/>
-              <Route path="/user" component={null}/>
-              <Route component={Sample}/>
-          </Switch>
-      </Router>
-  );
+    const classes = useStyles();
+
+    return (
+        <div id="appRouter">
+            <div className="appRoot">
+                <div className="appContent">
+                    <main>
+                        <Router>
+                            <CssBaseline/>
+                            <MainAppBar />
+                            <Switch>
+                                <Route exact path="/" component={Home}/>
+                                <Route path="/login" component={Login}/>
+                                <Route path="/maps" component={MapsApp}/>
+                                <Route path="/modding" component={ModdingApp}/>
+                                <Route path="/keys" component={KeysApp}/>
+                                <Route path="/user" component={null}/>
+                                <Route component={Home}/>
+                            </Switch>
+                        </Router>
+                    </main>
+                </div>
+                <Footer/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
