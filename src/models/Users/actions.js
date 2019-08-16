bf3c828826c6a4a3968b18396d70dd4ae25be2e3
Adam Brown <downtownadambrown@gmail.com>
2019-08-15 22:23:52 -0400

    builds User Redux model

---
+++ src/models/Users/actions.js
@@ -0,0 +1,7 @@
+export const GET_USERS_START = "GET_USERS_START";
+export const GET_USERS_ERROR = "GET_USERS_ERROR";
+export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS";
+
+export const getUsers = () => ({
+    type: GET_USERS_START,
+});
---
+++ src/models/Users/reducers.js
@@ -0,0 +1,47 @@
+import { Map } from "immutable";
+import _ from "lodash";
+
+import {
+    GET_USERS_START,
+    GET_USERS_ERROR,
+    GET_USERS_SUCCESS
+} from "./actions";
+
+const initialState = Map({
+    loading: false,
+    error: null,
+    data: null,
+});
+
+const actionsMap = {
+    [GET_USERS_START]: state => {
+        return state.merge(
+            Map({
+                loading: true,
+                error: null,
+            }),
+        );
+    },
+    [GET_USERS_ERROR]: (state, action) => {
+        return state.merge(
+            Map({
+                loading: false,
+                error: action.error.message,
+            }),
+        );
+    },
+    [GET_USERS_SUCCESS]: (state, action) => {
+        return state.merge(
+            Map({
+                loading: false,
+                error: null,
+                data: action.data,
+            }),
+        );
+    },
+};
+
+export default function reducer(state = initialState, action = {}) {
+    const fn = actionsMap[action.type];
+    return fn ? fn(state, action) : state;
+}
