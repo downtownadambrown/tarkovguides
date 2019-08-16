b9b17859e06ea9f14921f337094bdb01994c69f6
Adam Brown <downtownadambrown@gmail.com>
2019-08-16 01:13:48 -0400

    adds sentry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@material-ui/styles';
 // eslint-disable-next-line import/extensions
@@ -9,9 +8,10 @@ import theme from './theme/theme.js';
 import App from './App';
 import './css/index.css';
 import 'typeface-roboto';
-
+import * as Sentry from '@sentry/browser';
 import configureStore from './store/configureStore';
 
+Sentry.init({ dsn: "https://f196c7d628df4154ab2d434f8216ed80@sentry.io/1532258" });
 const store = configureStore();
 
 ReactDOM.render(
